ad2f992e9a08b9e27962e3ff04d9fa2f29e972e5
Aleš Kotnik <ales.kotnik@gmail.com>
2014-08-24 18:11:18 +0200

    Imporve comments.

--- run_analysis.R
+++ run_analysis.R
@@ -2,14 +2,14 @@
 # During development it was set to 200.
 NROWS<-8000
 
-# read feature names
+# Read feature names
 features.raw <- read.table("./data/features.txt", header=F, col.names=c("index", "name"))
 
-# some names are not unique so add column index to make them uniq
+# Some names are not unique so add column index to make them uniqe
 features.uniqe <- sprintf("%3d.%s", features.raw$index, features.raw$name)    
 
 # As we are interested only in mean and standard deviation data,
-# make list of thi features to later filter out all other data.
+# make list of these features to help filter out all other data.
 features.of.interest <- features[grep("\\-mean\\(|\\-std\\(", features)]
 
 # Fix some feature naming inconsistencies ad errors
@@ -22,14 +22,13 @@ renamed.features <- gsub("\\-Y", "Y", renamed.features)
 renamed.features <- gsub("\\-Z", "Z", renamed.features)
 renamed.features <- gsub("BodyBody", "Body", renamed.features)
 
-    
-# Prepare activity labels to label factored activiy during data import
+# Prepare activity labels to label numeric activiy data during data import
 activity_labels <- read.table("./data/activity_labels.txt", col.names=c("index", "name"))$name
 
-# Common function to read and merge subjects, activites and measurements data
+# Common function to read and merge subjects, activites and sensor measurements data
 # from training or test datasets.
 
-read_data <- function(subjects.file, activities.file, data.file) {
+read.my.data <- function(subjects.file, activities.file, data.file) {
     subjects <- read.table(subjects.file, nrows=NROWS, col.names="subject")
 
     activities <- factor(
@@ -37,7 +36,11 @@ read_data <- function(subjects.file, activities.file, data.file) {
         labels=activity_labels)
 
     data <- read.table(data.file, header=F, col.names=features.uniqe, nrows=NROWS)
+
+    # Select only features we are interested in
     data <- subset(data, select=features.of.interest)    
+
+    # ... and rename the columns to more consisten naming scheme
     names(data) <- renamed.features
 
     # make sure to bind smaller data frames to larger one for
@@ -46,15 +49,15 @@ read_data <- function(subjects.file, activities.file, data.file) {
 }
 
 
-# read and merge training and test data
+# Read and merge training and test data
 
-training.data <- read_data(
+training.data <- read.my.data(
     "./data/train/subject_train.txt", 
     "./data/train/y_train.txt", 
     "./data/train/X_train.txt"
     )
 
-test.data <- read_data(
+test.data <- read.my.data(
     "./data/test/subject_test.txt",
     "./data/test/y_test.txt",
     "./data/test/X_test.txt"
@@ -62,13 +65,13 @@ test.data <- read_data(
 
 merged.data = rbind(training.data, test.data)
 
-# create a tidy dataset from merged training and test data aggregated by subject and activity
+# Create a tidy dataset from merged training and test data aggregated by subject and activity
 tidy <- aggregate(.~subject+activity, merged.data, mean)
 tidy_ordered <- tidy[with(tidy, order(subject, activity)), ]
 write.table(tidy_ordered, "tidy.txt", row.name=FALSE)
 
 
-# prepare template file for writing code book.
+# Prepare template file for writing code book.
 
 codebook <- data.frame()
 for (name in names(tidy_ordered)) {
